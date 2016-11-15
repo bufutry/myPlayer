d8d9bb0f5a03d1d9e3039829c27008d0b24a8050
Fucker <290358009@qq.com>
2016-11-15 14:57:43 +0800

    Initial Commit

---
+++ palyer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  palyer
+//
+//  Created by 杨绍鑫 on 2016/11/15.
+//  Copyright © 2016年 macMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ palyer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  palyer
+//
+//  Created by 杨绍鑫 on 2016/11/15.
+//  Copyright © 2016年 macMini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
